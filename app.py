26cf311a4589f5a19f28190bd62100c98e831c4c
Edem Gold <78963277+EdemGold@users.noreply.github.com>
2023-11-27 15:14:27 +0100

    Update app.py with read in app feature

--- app.py
+++ app.py
@@ -1,26 +1,40 @@
 import streamlit as st
-import pandas as pd
-from pathlib import Path
+from pathlib import Path  
+from PyPDF2 import PdfFileWriter, PdfFileReader
 import uuid
 
 st.title("PDF Viewer App")
 
 pdfs = {}
 
-# Allow multiple uploads with a while loop
-uploaded = True
+# Allow multiple uploads 
+uploaded = True  
+
 while uploaded:
-    key = str(uuid.uuid4())
-    uploaded_file = st.file_uploader("Choose a PDF", type=['pdf'], key=key)
+
+    key = str(uuid.uuid4())  
+    uploaded_file = st.file_uploader("Choose PDF", type=['pdf'], key=key)
+
     if uploaded_file is not None:
-        file_details = {"FileName":uploaded_file.name,"FileType":uploaded_file.type}
+
+        file_details = {"FileName":uploaded_file.name,"FileType":uploaded_file.type}  
         pdfs[uploaded_file.name] = uploaded_file.read()
 
-    else:
-        uploaded = False
+        # Save to temp file
+        with open(Path("temp.pdf"), "wb") as f: 
+            f.write(uploaded_file.getbuffer())
 
-# Display all uploaded PDFs
-for name, data in pdfs.items():
+        # Read PDF
+        pdf_reader = PdfFileReader(Path("temp.pdf"))
+
+        # Display pages
+        for page_num in range(pdf_reader.numPages):
+            page = pdf_reader.getPage(page_num)
+            st.write(page)
+
+    else:
+        uploaded = False  
+
+# List uploaded PDFs
+for name in pdfs:
     st.write(name)
-    if st.checkbox(f'Show {name}'):
-        st.write(data)
